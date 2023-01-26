3b2ed77454d82ac0e49121b92cd3181cce263c13
Vinicius Ouro Preto Ribeiro <viniciusouro73@gmail.com>
2023-01-26 13:11:21 -0300

    CSV & Random
    Push Projetos CSV & Random com Python

---
+++ CSV/main.py
@@ -0,0 +1,41 @@
+"""
+Comma Separated Values - CSV (Valores separados por vírgula)
+É um formato de dados muito usado em tabelas (Excel, Google Sheets), bases de
+dados, clientes de e-mail, etc...
+"""
+
+import csv
+
+with open('clientes.csv', 'r') as Arquivo:
+    dados = [x for x in csv.DictReader(Arquivo)]
+
+with open('clientes2.csv', 'w') as Arquivo:
+    escreve = csv.writer(
+        Arquivo,
+        delimiter=',',
+        quotechar='"',
+        quoting=csv.QUOTE_ALL
+    )
+
+    chaves = dados[0].keys()
+    chaves = list(chaves)
+    escreve.writerow(
+        [
+            chaves[0],
+            chaves[1],
+            chaves[2],
+            chaves[3],
+
+        ]
+    )
+
+    for dado in dados:
+        escreve.writerow(
+            [
+                dado['Nome'],
+                dado['Sobrenome'],
+                dado['E-mail'],
+                dado['Telefone']
+            ]
+
+        )
